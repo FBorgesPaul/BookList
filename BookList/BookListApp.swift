106435ac70210c76e9c9c06a81461d9b69d570f0
Fernando Borges Paul <97313100+FBorgesPaul@users.noreply.github.com>
2022-05-11 17:11:16 -0500

    Initial Commit

---
+++ BookList/BookListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookListApp.swift
+//  BookList
+//
+//  Created by Fernando Borges on 11/05/22.
+//
+
+import SwiftUI
+
+@main
+struct BookListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
